452b58b2fd9d0343dc1d1d9900d4608b47eaaae8
Markus Avonius <mavonius@gmail.com>
2018-09-24 22:04:10 +0300

    Update 01-run.sh
    salt to config minion_id and minion how to gen to image

--- stage3/00-install-packages/01-run.sh
+++ stage3/00-install-packages/01-run.sh
@@ -6,3 +6,21 @@ update-alternatives --install /usr/bin/x-www-browser \
 update-alternatives --install /usr/bin/gnome-www-browser \
   gnome-www-browser /usr/bin/chromium-browser 86
 EOF
+
+
+on_chroot << EOF
+echo '#!/bin/bash
+
+file="/etc/salt/minion_id"
+if [ -f "$file" ]
+then
+	echo "$file found."
+else
+	echo "$file not found."
+	uuid=$(uuidgen)
+	echo "raspberry_pi_3_09_2018_${uuid}" > /etc/salt/minion_id
+fi' > /saltminiongenid1.sh
+
+mkdir /etc/salt
+echo "master: HOSTNAME" > /etc/salt/minion
+EOF
